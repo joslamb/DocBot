fa84e878d4d240f90066afb98fb9c09adc137f31
joscrd <j194cc@gmail.com>
2025-06-16 18:28:45 -0700

    preparacion de segumiento

---
+++ src/main/java/com/lamb/docbot/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.lamb.docbot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "App is running!";
+    }
+}
---
+++ src/test/java/com/lamb/docbot/DocBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamb.docbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
